d42d7a8992971f75d190e3d72323711b521263ea
Merter Sualp <merter.sualp@gmail.com>
2016-07-21 09:30:16 +0300

    Chaos string message is fixed

--- NewYearChaos.java
+++ NewYearChaos.java
@@ -22,7 +22,7 @@ public class NewYearChaos {
     int count = 0;
     for (int i=0; i<nums.length-1; i++) {
       if (nums[i] > i+3) {
-        return "TOO CHAOTIC";
+        return "Too chaotic";
       }
       for (int j=i+1; j<nums.length; j++)
         if (nums[i] > nums[j])
